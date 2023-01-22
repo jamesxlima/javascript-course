8712e534a48380376ff795e9dc9a357a98d58c9b
James <116779226+jamesxlima@users.noreply.github.com>
2023-01-21 21:25:59 -0500

    Add files via upload

---
+++ js_lesson1/js/script.js
@@ -0,0 +1,4 @@
+console.log("Hello World!");
+console.log(typeof "James");
+console.log(typeof 42);
+console.log(typeof true);
---
+++ js_lesson2/js/script.js
@@ -0,0 +1,31 @@
+// String Methods & Properties
+
+// String
+const myVariable = "Mathematics";
+
+// The length property
+console.log(myVariable.length);
+
+// Character At
+console.log(myVariable.charAt(0));
+
+// Index Of 
+console.log(myVariable.indexOf("mat"));
+
+// Last Index Of 
+console.log(myVariable.lastIndexOf("ema"));
+
+// Slice
+console.log(myVariable.slice(5));
+
+// To Uppercase
+console.log(myVariable.toUpperCase());
+
+// To Lowercase
+console.log(myVariable.toLowerCase());
+
+// Includes
+console.log(myVariable.includes("mat"));
+
+// Split
+console.log(myVariable.split("e"));
