9af53f13cf4804e1b3edd6675373e732b6523975
James Lima <jamesxlima@gmail.com>
2023-01-23 00:41:30 -0500

    Lesson Added

--- js_lesson4/js/script.js
+++ js_lesson4/js/script.js
@@ -1,1 +1,36 @@
 // Math Methods
+
+// Math.PI property
+console.log(Math.PI);
+// Expected output: 3.141592653589793
+
+// Math.trunc() method returns the integer part of a number by removing any fractional digits.
+console.log(Math.trunc(123.456));
+// Expected output: 123
+
+// Math.round() method returns the value of a number rounded to the nearest integer.
+console.log(Math.round(0.9));
+// Expected output: 1
+
+// Math.ceil() method always rounds up and returns the smaller integer greater than or equal to a given number.
+console.log(Math.ceil(7.004));
+// Expected output: 8
+
+// Math.floor() method always rounds down and returns the largest integer less than or equal to a given number.
+console.log(Math.floor(5.95));
+// Expected output: 5
+
+// Math.pow() method returns the value of a base raised to a power.
+console.log(Math.pow(7, 2));
+// Expected output: 49
+
+// Math.min() method returns the smallest of the numbers given as input parameters.
+console.log(Math.min(100, 7, 81));
+// Expected output: 7
+
+// Math.max() method returns the largest of the numbers given as input parameters.
+console.log(Math.max(100, 7, 81));
+// Expected output: 100
+
+// Math.random() method returns a random number between 0 (inclusive),  and 1 (exclusive).
+console.log(Math.random())
