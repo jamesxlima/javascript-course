c5c74109ab8a15fd26b8f968a0fc228b629858b7
James Lima <jamesxlima@gmail.com>
2023-01-23 01:16:25 -0500

    Updated script.js

--- js_lesson1/js/script.js
+++ js_lesson1/js/script.js
@@ -1,4 +1,15 @@
+// JavaScript Message
 console.log("Hello World!");
+// Expected Output: Hello World!
+
+// String. A JavaScript string stores a series of characters.
 console.log(typeof "James");
+// Expected Output: string
+
+// Number. JavaScript number values represent floating-point numbers.
 console.log(typeof 42);
+// Expected Output: number
+
+// Boolean. JavaScript booleans can have one of two values: true or false.
 console.log(typeof true);
+// Expected Output: boolean
