4df82c80560b179d09e1cf3202789631fbfe517b
James Lima <jamesxlima@gmail.com>
2023-01-25 09:31:29 -0500

    Lesson Added

--- js_lesson12/js/script.js
+++ js_lesson12/js/script.js
@@ -1,4 +1,4 @@
-// var, let, and const
+// var, let, and const in JavaScript Scope
 
 // Use "const" whenever you can
 // Use "let" when you know you're going to re-assign a value
@@ -21,3 +21,27 @@ function myFunc() {
 myFunc();
 
 // If statements, for loops, and switch statements have block scopes.
+
+// A 2nd example of global, function, and block scopes.
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${z}`);
+
+function myFunc2() {
+    var x = 10;
+    const z = 5;
+  console.log(`function: ${x}`);
+  console.log(`function: ${y}`);
+  console.log(`function: ${z}`);
+
+  {
+      var x =11; // var will be applied to function outside the scope (curly brackets). "Function Scoped"
+      const z = 6; // const is only applied inside the scope (curly brackets). "Block Scoped"
+    console.log(`block: ${x}`);
+    console.log(`block: ${y}`);
+    console.log(`block: ${z}`);
+  }
+
+}
+
+myFunc2();
