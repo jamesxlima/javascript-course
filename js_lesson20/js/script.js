f1791d1498bb898012f7814577f6dbef2e8850e8
James Lima <jamesxlima@gmail.com>
2023-02-02 23:01:33 -0500

    Update script.js

--- js_lesson20/js/script.js
+++ js_lesson20/js/script.js
@@ -28,3 +28,27 @@ h2.addEventListener("click", (event) => {
 })
 
 // Listening for the readystatechange event
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        console.log("readyState: complete");
+        initApp();
+    }
+});
+
+const initApp = () => {
+    const view = document.querySelector("#view2");
+    const div = view.querySelector("div");
+    const h2 = view.querySelector("h2");
+    
+    view.addEventListener("click", (event) => {
+        view.style.backgroundColor = "purple";
+    })
+    
+    div.addEventListener("click", (event) => {
+        div.style.backgroundColor = "blue";
+    })
+    
+    h2.addEventListener("click", (event) => {
+        event.target.textContent = "Clicked";
+    })
+}
