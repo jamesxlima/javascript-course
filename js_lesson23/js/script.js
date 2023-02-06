3354055590cb8e1e3f64120d338f6f4f158ad0a1
James Lima <jamesxlima@gmail.com>
2023-02-05 22:59:17 -0500

    Update script.js

--- js_lesson23/js/script.js
+++ js_lesson23/js/script.js
@@ -2,5 +2,6 @@
 
 /* A higher order function is a function that does at least one of the following:
 ** Takes one or more functions as an argument (parameter)
-** Reutrns a function as a result
+** Returns a function as the result
+
 */
