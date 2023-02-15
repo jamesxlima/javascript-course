65af5b1789ab6ca45aee5d13f253a25daaab1ed1
James Lima <jamesxlima@gmail.com>
2023-02-15 17:23:52 -0500

    Update script.js

--- js_lesson24/js/script.js
+++ js_lesson24/js/script.js
@@ -8,11 +8,11 @@ function firstFunction(parameters, callback) {
 }
 
 // AKA "Callback Hell"
-firstFunction(param, function() {
+/* firstFunction(param, function() {
     secondFunction(para, function() {
         thirdFunction(para, function() {});
     });
-});
+}); */
 
 
 // Promises
@@ -27,4 +27,46 @@ const myPromise = new Promise((resolve, reject) => {
     };
 });
 
-// 
+console.log(myPromise);
+
+myPromise
+.then(value => {
+    console.log(value);
+})
+.then(newValue => {
+    console.log(newValue);
+})
+.catch(err => {
+    console.error(err);
+})
+
+const myNextPromise = new Promise((resolve, reject) => {
+    setTimeout(function() {
+        resolve("myNextPromise resolved!");
+    }, 3000);
+});
+
+myNextPromise.then(value => {
+    console.log(value);
+});
+
+myPromise.then(value => {
+    console.log(value);
+});
+
+// Using the Fetch API 👇
+const users = fetch("https://jsonplaceholder.typicode.com/users");
+
+// Pending
+console.log(users);
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => {
+    console.log(response);
+    return response.json();
+})
+.then(data => {
+    data.forEach(user => {
+        console.log(user);
+    })
+});
